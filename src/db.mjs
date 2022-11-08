8ef787eba7527f1b292b2d9b5102889d0df15b9a
himanshilalwani <himanshi.lalwani123@gmail.com>
2022-11-07 23:53:45 -0500

    deployment part 4

--- src/db.mjs
+++ src/db.mjs
@@ -1,4 +1,7 @@
 import { mongoose } from 'mongoose'
+import fs from 'fs';
+import path from 'path';
+import url from 'url';
 
 // budgets
 const BudgetSchema = new mongoose.Schema({
@@ -33,4 +36,21 @@ mongoose.model('Budgets', BudgetSchema);
 mongoose.model('Expenses', ExpenseSchema);
 mongoose.model('Wallets', WalletSchema);
 
-mongoose.connect('mongodb://localhost/final');
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+let dbconf;
+if (process.env.NODE_ENV === 'PRODUCTION') {
+    // if we're in PRODUCTION mode, then read the configration from a file
+    // use blocking file io to do this...
+    const fn = path.join(__dirname, 'config.json');
+    const data = fs.readFileSync(fn);
+
+    // our configuration file will be in json, so parse it and set the
+    // conenction string appropriately!
+    const conf = JSON.parse(data);
+    dbconf = conf.dbconf;
+} else {
+    // if we're not in PRODUCTION mode, then use
+    dbconf = 'mongodb://localhost/hl3937';
+}
+
+mongoose.connect(dbconf);
